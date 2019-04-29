9c01df8f908495625a05a4cb383a2ad5686f85b2
selfly <selfly@foxmail.com>
2019-04-29 09:55:44 +0800

    修改Transient为属性注解

--- sonsure-dumper-core/src/main/java/com/sonsure/dumper/core/annotation/Transient.java
+++ sonsure-dumper-core/src/main/java/com/sonsure/dumper/core/annotation/Transient.java
@@ -3,13 +3,13 @@ package com.sonsure.dumper.core.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Created by liyd on 2015-12-31.
  */
-@Target({ METHOD })
+@Target({ FIELD })
 @Retention(RUNTIME)
 public @interface Transient {
 }
